d4cf5ac8b95a9bbc7921ab087ceba3aa1500d315
parkinyeop <parkinyeop@naver.com>
2022-09-16 20:30:21 +0900

    enemy 애니메이션 셋팅 ver 0.1
    1,2,3,4 로 애니메이션 출력

--- Temp1/Assets/Scripts/Enemy.cs
+++ Temp1/Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public enum Type { Orc, Skelleton, Mage, Shell, Boss}
+    public enum Type { Orc, Skelleton, Mage, Shell, Boss }
     public Type enemyType;              //Attack 메서드에서 공격 타입을 설정하기 위해
     public int curHealth;
     public int maxHealth;
@@ -22,28 +22,90 @@ public class Enemy : MonoBehaviour
     public bool isAttack;
     public bool isDead;
 
-    
+    bool isAttackTest = false;
+    bool isWalkTest = false;
+    bool isGetHitTest = false;
+    bool isDieTest = false;
+
+
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();    
+        anim = GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
         boxCollider = GetComponent<BoxCollider>();
     }
 
     private void Start()
     {
-        meleeAttack.enabled = false;        //밀리 어택 컬리젼을 꺼 둠
-        //StartCoroutine(Attack());           //코루틴 테스트용 임시 코드
+        //meleeAttack.enabled = false;        //밀리 어택 컬리젼을 꺼 둠
+        //StartCoroutine(Action());           //코루틴 테스트용 임시 코드
     }
-    void Thinking()
+
+    private void Update()
     {
+        if (Input.GetKeyDown("1") && !isAttackTest)
+            StartCoroutine(Action1());
 
+        if (Input.GetKeyDown("2") && !isWalkTest)
+            StartCoroutine(Action2()); 
+        
+        if (Input.GetKeyDown("3") && !isGetHitTest)
+            StartCoroutine(Action3());
+        
+        if (Input.GetKeyDown("4") && !isDieTest)
+            StartCoroutine(Action4());
     }
 
-    //IEnumerator Attack()
+    IEnumerator Action1()
+    {
+        anim.SetBool("isAttack", true);
+        isAttackTest = true;
+        yield return new WaitForSeconds(0.8f);
+
+        anim.SetBool("isAttack", false);
+        isAttackTest = false;
+        StopCoroutine(Action1());
+    }
+    IEnumerator Action2()
+    {
+        anim.SetBool("isWalk", true);
+        isWalkTest = true;
+        yield return new WaitForSeconds(0.8f);
+
+        anim.SetBool("isWalk", false);
+        isWalkTest = false;
+        StopCoroutine(Action2());
+    }
+    IEnumerator Action3()
+    {
+        isGetHitTest = true;
+        anim.SetBool("isGetHit", true);
+        yield return new WaitForSeconds(0.8f);
+
+        anim.SetBool("isGetHit", false);
+        isGetHitTest = false;
+        StopCoroutine(Action3());
+    }
+    IEnumerator Action4()
+    {
+        anim.SetTrigger("doDie");
+        isDieTest = true;
+        yield return new WaitForSeconds(0.8f);
+
+        isDieTest = false;
+        StopCoroutine(Action4());
+    }
+    //IEnumerator Action()
     //{
     //    meleeAttack.enabled = true;         //밀리어택의 컬리젼을 켜 줌
     //    anim.SetBool("isAttack", true);
+    //    int actionNum = Random.Range(0, 4);
+    //    switch (actionNum)
+    //    {
+    //        case 0:
+    //            anim.SetBool("isAttack", true);
+    //            break;
+    //    }
     //}
 }

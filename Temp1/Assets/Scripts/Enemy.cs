87257bcd53be49d93f957dba283141916994586a
parkinyeop <parkinyeop@naver.com>
2022-09-19 17:38:28 +0900

    Enemy Move Applied

--- Temp1/Assets/Scripts/Enemy.cs
+++ Temp1/Assets/Scripts/Enemy.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,12 +12,16 @@ public class Enemy : MonoBehaviour
     public int curHealth;
     public int maxHealth;
     public Transform target;            //플레이어 타겟
+    public float moveSpeed = default;             //이동 속도
+    public float rotSpeed = 1.0f;
+
+    Vector3 targetDirection;
 
     public GameObject projectile;       //원거리용 발사체
     public BoxCollider meleeAttack;     //밀리 어택용 컬리젼 박스
     public NavMeshAgent nav;            //네비 매쉬를 사용
-    public Rigidbody rigid;
-    public BoxCollider boxCollider;     //피격에 사용되는 기본 컬리젼
+    public Rigidbody rb;
+    public CapsuleCollider capsuleCollider;     //피격에 사용되는 기본 컬리젼
     public Animator anim;
 
     public bool isChase;                //타겟을 향해 이동중
@@ -30,33 +36,57 @@ public class Enemy : MonoBehaviour
 
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
-        boxCollider = GetComponent<BoxCollider>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        meleeAttack = GetComponent<BoxCollider>();
+        target = target.GetComponent<Transform>();
     }
 
     private void Start()
     {
         //meleeAttack.enabled = false;        //밀리 어택 컬리젼을 꺼 둠
         //StartCoroutine(Action());           //코루틴 테스트용 임시 코드
+
     }
 
     private void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        targetDirection = (target.position - transform.position).normalized;
+        AnimationTest();
+        rb.MovePosition(transform.position + targetDirection * Time.deltaTime * moveSpeed);
+        transform.LookAt(target);
+        isChase = true;
+        anim.SetBool("isWalk", true);
+
+    }
+
+    private void AnimationTest()
     {
         if (Input.GetKeyDown("1") && !isAttackTest)
             StartCoroutine(Action1());
 
         if (Input.GetKeyDown("2") && !isWalkTest)
-            StartCoroutine(Action2()); 
-        
+            StartCoroutine(Action2());
+
         if (Input.GetKeyDown("3") && !isGetHitTest)
             StartCoroutine(Action3());
-        
+
         if (Input.GetKeyDown("4") && !isDieTest)
             StartCoroutine(Action4());
     }
 
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
     IEnumerator Action1()
     {
         anim.SetBool("isAttack", true);
